acea1e09ed5bb5bd9bd9dfd3d1a9682993bae25f
aad05 <abduvoitovasadbek3@gmail.com>
2023-02-23 22:24:42 +0500

    first commit

--- src/components/EmptyPlace/OrdinaryRooms/SecondBuilding/Mapping/Room/UserModal/Editing/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Wrapper = styled.div`
-  padding-top: 20px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
--- src/images.d.ts
+++
@@ -1,6 +0,0 @@
-// src/types/images.d.ts
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.gif";
-declare module "*.svg";
